ba0e0474267b44bb754e0ca9d9ae5b157683b930
Adam O'Neil <adamosoftware@gmail.com>
2022-12-09 06:57:40 -0500

    started on some updates, new component

--- GitHubApiClient/Models/Settings.cs
+++ GitHubApiClient/Models/Settings.cs
@@ -1,10 +1,8 @@
-﻿#nullable disable
-
-namespace GitHubApiClient.Models
+﻿namespace GitHubApiClient.Models
 {
     public class Settings
     {
-        public string UserName { get; set; }
-        public string Token { get; set; }
+        public string UserName { get; set; } = default!;
+        public string Token { get; set; } = default!;
     }
 }
